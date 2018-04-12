8bff67b89e5d2a3c0e19cb90c59cac07f97b2aa4
Dennis Trate <32754716+pseudodennis@users.noreply.github.com>
2018-04-12 00:49:48 -0400

    Update Dignity.java
    I think this main method is done.

--- dignity/Dignity.java
+++ dignity/Dignity.java
@@ -1,41 +1,51 @@
-
 public class Dignity
 {
-  public static void main(String[] args)
-  {
+    public static void main(String[] args)
+    {
 
-    boolean gamewin = false;
-    boolean tiegame = false;
-    
-    /**
-     *  construct players
-     */
-    CPU player1 = new CPU();
-    CPU player2 = new CPU();
-    // Human player3 = new Human();
-    // NN player4 = new NN();
+        /**
+         *  construct players
+         */
+        CPU player1 = new CPU();
+        CPU player2 = new CPU();
+        // Human player3 = new Human();
+        // NN player4 = new NN();
 
 
-    /**
-     *  construct gameboard
-     */
-    Board board = new Board(6, 7);
+        /**
+         *  construct gameboard
+         */
+        Board board = new Board(6, 7);
 
 
-    /**
-     *  play game
-     */
-    while (!gamewin && !tiegame)
-    {  
-      board = board.setState(player1.move(board));
-      gamewin = board.win();
-      tiegame = board.tie();
-      
-      board = board.setState(player2.move(board));  
-      gamewin = board.win();
-      tiegame = board.tie();
-    }
+        /**
+         *  play game
+         */
+
+        while (!board.isGameOver())
+        {
+            // player one moves
+            while (board.isPlayer1Turn())
+            {
+                board.addPiece(player1.move(board.getBoardState()));
+                /**
+                 * player1.move() accepts the current board array as a parameter
+                 * and returns an int specifying the column where it wants to move.
+                 * board.addPiece() accepts that int as the column in which to place the piece.
+                 * The board will validate and check for a win after every move.
+                 */
+            }
+
+            // then player two moves
+            while (!board.isPlayer1Turn())
+            {
+                board.addPiece(player2.move(board.getBoardState()));
+            }
+
+        }
+
+        System.out.println(board.getFinalScore())
 
 
-  } // end of main()
+    } // end of main()
 } // end of dignity

7c804ec285f7d3f27907ac246f530726f7f47287
hjpoe <35583004+hjpoe@users.noreply.github.com>
2018-04-12 15:23:41 -0400

    Update the win method
    Uses the code tested in VictoryLap.java in the win method to check for a win.

--- dignity/Board.java
+++ dignity/Board.java
@@ -61,8 +61,122 @@ public class Board
 
 	public boolean win()
 	{
+		int nRows = 6;  // the number of rows in the board
+		int nCols = 7;  // the number of cols in the board
+		int iRow = 0;   // row index number for incrementing
+		int iCol = 0;   // col index number for incrementing
+		int cl = 4;     // length of chain for indexing
 		boolean win = false;
-		// Check algorithms go here.
+		
+// check for horizontal wins
+
+		// move through each cell, unless the cells are within 4 spaces of nCols
+
+		// move down the rows
+		for (iRow = 0; iRow < nRows && !win; iRow++)
+		{
+			// move from left to right across row
+			for (iCol = 0; iCol <= nCols - cl && !win; iCol++)
+			{
+				// grab the starting cell value and put in hashset
+				int cellVal = board[iRow][iCol];
+				if (cellVal != 0)
+				{
+					Set<Integer> hs = new HashSet<>(Arrays.asList(cellVal));
+					win = true;
+					// iterate through the next three cells, if add==true, win=false
+					for (int i = 1; i <= 3 ; i++)
+					{
+						if (hs.add(board[iRow][iCol + i])==true)
+						win = false;
+					}
+				}
+			} // end of moving through columns
+		}
+
+
+// check for vertical wins
+
+		// move through each cell, unless the cells are within 4 spaces of nCols
+
+		// move across all the cols
+		for (iCol = 0; iCol < nCols && !win; iCol++)
+		{
+			// move from top to bottom through each row in col
+			for (iRow = 0; iRow <= nRows - cl && !win; iRow++)
+			{
+				// grab the starting cell value and put in hashset
+				int cellVal = board[iRow][iCol];
+				if (cellVal != 0)
+				{
+					Set<Integer> hs = new HashSet<>(Arrays.asList(cellVal));
+					win = true;
+
+					// iterate through the next three cells, if add==true, win=false
+					for (int i = 1; i <= 3; i++)
+					{
+						if (hs.add(board[iRow + i][iCol]))
+						win = false;
+					}
+				}
+			} // end of moving through columns
+		}
+
+
+// check for downhill diag wins
+
+		// move through each cell, unless the cells are within 4 spaces of nCols
+
+		// move across the cols within range
+		for (iCol = 0; iCol <= nCols-cl && !win; iCol++)
+		{
+			// move from top to bottom through row in col in range
+			for (iRow = 0; iRow <= nRows - cl && !win; iRow++)
+			{
+				// grab the starting cell value and put in hashset
+				int cellVal = board[iRow][iCol];
+				if (cellVal != 0)
+				{
+					Set<Integer> hs = new HashSet<>(Arrays.asList(cellVal));
+					win = true;
+
+					// iterate through the next three cells, if add==true, win=false
+					for (int i = 1; i <= 3; i++)
+					{
+						if (hs.add(board[iRow + i][iCol + i]))
+						win = false;
+					}
+				}
+			} // end of moving through columns
+		}
+
+// check for uphill diag wins
+
+		// move through each cell, unless the cells are within 4 spaces of nCols
+
+		// move down the rows, starting within range
+		for (iRow = cl-1; iRow < nRows && !win; iRow++)
+		{
+			// move from top to bottom through row in col in range
+			for (iCol = 0; iCol <= nCols - cl && !win; iCol++)
+			{
+				// grab the starting cell value and put in hashset
+				int cellVal = board[iRow][iCol];
+				if (cellVal != 0)
+				{
+					Set<Integer> hs = new HashSet<>(Arrays.asList(cellVal));
+					win = true;
+
+					// iterate through the next three cells, if add==true, win=false
+					for (int i = 1; i < cl; i++)
+					{
+						if (hs.add(board[iRow - i][iCol + i]))
+						win = false;
+					}
+				}
+			} // end of moving through columns
+		}
+		
 		return win;
 	} // end of win
 

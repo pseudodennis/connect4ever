1789e586e991fa0390fc09f9a6a2b53738ceeeb3
Dennis Trate <32754716+pseudodennis@users.noreply.github.com>
2018-04-12 21:07:00 -0400

    Update Dignity.java
    THIS VERSION WORKS

--- dignity/Dignity.java
+++ dignity/Dignity.java
@@ -11,50 +11,54 @@ public class Dignity
         // Human player3 = new Human();
         // NN player4 = new NN();
 
+	    Board board;
 
-        /**
-         *  construct gameboard
-         */
-        Board board = new Board(6, 7);
-        System.out.println(board.toString());
+	    do
+	    {
 
-        /**
-         *  play game
-         */
+		    /**
+		     *  construct gameboard
+		     */
+		    board = new Board(6, 7);
+		    System.out.println(board.toString());
 
-        while (!board.isGameOver())
-        {
-            // player one moves
-            while (board.isPlayer1Turn())
-            {
-                board.addPiece(player1.move(board.getBoardState()));
-                /**
-                 * player1.move() accepts the current board array as a parameter
-                 * and returns an int specifying the column where it wants to move.
-                 * board.addPiece() accepts that int as the column in which to place the piece.
-                 * The board will validate and check for a win after every move.
-                 */
+		    /**
+		     *  play game
+		     */
+
+		    while (!board.isGameOver())
+		    {
+			    // player one moves
+			    if (board.isPlayer1Turn() && !board.isGameOver())
+			    {
+				    board.addPiece(player1.move(board.getBoardState()));
+				    /**
+				     * player1.move() accepts the current board array as a parameter
+				     * and returns an int specifying the column where it wants to move.
+				     * board.addPiece() accepts that int as the column in which to place the piece.
+				     * The board will validate and check for a win after every move.
+				     */
 
 
-            }
-	        System.out.println(board.toString());
-	        Thread.sleep(500);
+			    }
+			    // System.out.println(board.toString());
+			    Thread.sleep(00);
 
-            // then player two moves
-            while (!board.isPlayer1Turn())
-            {
-                board.addPiece(player2.move(board.getBoardState()));
-            }
-	        System.out.println(board.toString());
+			    // then player two moves
+			    if (!board.isPlayer1Turn() && !board.isGameOver())
+			    {
+				    board.addPiece(player2.move(board.getBoardState()));
+			    }
 
-	        Thread.sleep(500);
+			    System.out.println(board.toString());
+			    Thread.sleep(00);
 
-        }
+		    }
 
-	    System.out.println(board.toString());
 
-	    System.out.println(board.getFinalScore());
+		    System.out.println(board.getFinalScore());
 
+	    } while (!board.isTiegame());
 
     } // end of main()
 } // end of dignity

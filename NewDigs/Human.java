7ee710d962bca8be8a78f192653164fd9511713e
hjpoe <35583004+hjpoe@users.noreply.github.com>
2018-04-26 19:08:10 -0400

    Update Human.java
    Added documentation comments

--- NewDigs/Human.java
+++ NewDigs/Human.java
@@ -1,14 +1,29 @@
 
+/**
+ * The Human class gets a column number from the hardware
+ * to pass it to the main class. 
+ */
+
 public class Human {
+	
+	/**
+	 * @param playerBoard A two dimentional array that holds the
+	 * board. We don't use this.
+	 * @return The column that the player went in.
+	 */
 	
 	public int move(int[][] playerBoard) {
 		int colPick;
 		
 		do {
+			/**
+			 * Get the location of the piece the player put in
+			 * from the arduino
+			 */
 			colPick = hardware_connect.getNum();
 		} while(colPick == 0);
 		
 		return (colPick - 1);
-	}
+	} // end move method
 
-}
+} // end human class

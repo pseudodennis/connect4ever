c22debc3d6fb8edc8c337b94d948fc004ab9f48d
hjpoe <35583004+hjpoe@users.noreply.github.com>
2018-04-25 21:12:02 -0400

    Update Human.java

--- NewDigs/Human.java
+++ NewDigs/Human.java
@@ -1,8 +1,14 @@
 
 public class Human {
 	
-	public int getHumanMove() {
-		return hardware_connect.getNum();
+	public int move(int[][] playerBoard) {
+		int colPick;
+		
+		do {
+			colPick = hardware_connect.getNum();
+		} while(colPick == 0);
+		
+		return (colPick - 1);
 	}
 
 }

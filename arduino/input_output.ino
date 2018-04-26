39045beb3935699f6e5a27b7f9b117a6ca43d289
flyingoatmeal <josephjoejr@gmail.com>
2018-04-26 19:45:25 -0400

    Add files via upload

---
+++ arduino/input_output.ino
@@ -0,0 +1,133 @@
+int starter = 0;
+int numOut = 0;
+int numIn;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(7, OUTPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+  LED();
+  digitalWrite(LED_BUILTIN, HIGH);
+  do
+  {
+    starter = Serial.parseInt();
+  }while (starter != 33 && starter != 77);
+    if(starter == 33)
+  {
+       Serial.println("computer");
+  }
+  else if (starter == 77)
+  {
+     Serial.println("human");
+  }
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (starter == 77) //human starts
+  {
+      
+    numIn = 0;
+    in();
+    numOut = 0;
+    out();
+  }
+  else // computer starts
+  {
+    numOut = 0;
+    out();
+    numIn = 0;
+    in();
+  }
+ 
+}
+void out() 
+{
+   do{
+  numOut = Serial.parseInt();
+  for(int i = 1; i <= numOut; i++)
+  {
+  if (numOut == 50)
+  {
+    Exit50();
+  }
+  else
+  {
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(7, LOW);
+  delay(500);
+  }
+  }
+  }while(numOut == 0);
+ }
+void in()
+{
+    
+  do{
+   if(digitalRead(3) == HIGH && digitalRead(4) == LOW)
+  {
+    numIn = 1;
+  }
+  else if(digitalRead(4) == HIGH && digitalRead(3) == LOW)
+  {
+    numIn = 2;
+  }
+  else if(digitalRead(4) == HIGH && digitalRead(3) == HIGH)
+  {
+    numIn = 3;
+  }
+  else if(digitalRead(5) == HIGH)
+  {
+    numIn = 4;
+  }
+  else if(digitalRead(6) == HIGH)
+  {
+    numIn = 5;
+  }
+  else if(digitalRead(8) == HIGH)
+  {
+    numIn = 6;
+  }
+ /* else if (digitalRead(12) == HIGH)
+  {
+    numIn = 7;
+  } */
+  else
+  {
+    numIn = 0;
+  }
+  Serial.println(numIn);
+  delay(1);
+  }while(numIn == 0);
+}
+
+void LED()
+{
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);  // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
+
+void Exit50()
+{
+  Serial.println("exiting");
+    Serial.println("99");
+    delay(22);
+    LED();
+    exit(0);
+}
+
+
---
+++ arduino/relay_motors.ino
@@ -0,0 +1,72 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+bool on = false;
+bool sw1 = false;
+bool sw2 = false;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(9, HIGH);
+  digitalWrite(11, HIGH);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //sw1 = digitalRead(3);
+ // sw2 = digitalRead(4);
+  if( digitalRead(3) == HIGH || digitalRead(4) == HIGH)
+  {
+    on = !on;
+   /* if(on == false)
+    {
+      on = true;
+    }
+    else //if (on == true)
+    {
+      on = false;
+    } */
+  }
+  Serial.println(on);
+  while(on == true && digitalRead(3) == LOW && digitalRead(4) == LOW)
+  {
+  digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(150);                       // wait for a second
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+  delay(153);
+  }
+    
+      digitalWrite(9, HIGH);
+      digitalWrite(11, HIGH);
+    
+  }

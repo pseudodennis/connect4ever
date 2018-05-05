5a477b81505e8c5ac86d8d7e8ea29c993b70f591
pseudodennis <pseudodennis.social@gmail.com>
2018-05-05 16:57:12 -0400

    Updated so the agent trains against the Solver

--- java/connect4ever/C4MDP.java
+++ java/connect4ever/C4MDP.java
@@ -23,8 +23,9 @@ public class C4MDP implements MDP<C4MDP.GameObservation, Integer, DiscreteSpace>
     protected       double scaleFactor = 1; // the amount by which to multiply the reward
     C4CPU opponent;
     int stepCount;
+    boolean v = false;		// verbose option to control printing out every move
 
-    // for the Solver
+    // for the Solver classes
     int columnPosition;
     SolverState theBoard;
     SolverMinMax computerPlayer;
@@ -71,8 +72,11 @@ public class C4MDP implements MDP<C4MDP.GameObservation, Integer, DiscreteSpace>
        c4board.updateLegalActions();
        actions = new int[c4board.getLegalActions().length];
        c4board.reset();
-       return new GameObservation(c4board);
+		//  reset the Solver board
+       		computerPlayer = new SolverMinMax(SolverState.O);
+       		theBoard = new SolverState();
 
+       return new GameObservation(c4board);
     }
 
     @Override
@@ -91,65 +95,87 @@ public class C4MDP implements MDP<C4MDP.GameObservation, Integer, DiscreteSpace>
     // https://github.com/deeplearning4j/gym-java-client/blob/master/src/main/java/org/deeplearning4j/gym/StepReply.java
     public StepReply<GameObservation> step(Integer action)
     {
-        double r = -0.01;
-        this.stepCount++;
-        // check is the action is legal, if not skip both players and set reward to a very large negative number
+    		double r = -0.1;		// default reward value if no one wins this turn; just a small incentive to win quickly if possible
+			this.stepCount++;		// running total of steps/iterations to track training progress
 
-        int[] legalActions = c4board.getLegalActions();
-        Set<Integer> legalSet = new HashSet<Integer>();
-            for (int i=0; i<legalActions.length; i++)
-            {
-                legalSet.add(legalActions[i]);
-            }
+		//  check is the action is legal with a HashSet(!)
+        	int[] legalActions = c4board.getLegalActions();
+        	Set<Integer> legalSet = new HashSet<Integer>();
+			for (int i=0; i<legalActions.length; i++)
+			{
+				legalSet.add(legalActions[i]);
+			}
+			// System.out.println(legalSet.toString()); 						// print legal moves for debugging
 
-        if (!legalSet.add(action))
-        {
-            c4board.setDqnValid(c4board.getDqnValid()+1);
-            if (!c4board.isGameOver())
-            {
-                c4board.addPiece(action, 1);
-                actions = new int[c4board.getLegalActions().length];
-                actions = c4board.getLegalActions();
+			if (!legalSet.add(action))											// if the move is legal... (proceed through a full 2-player turn, otherwise, .step again)
+			{
+				/*
+					DQN moves first
+				*/
+				c4board.setDqnValid(c4board.getDqnValid()+1);					// increment the number of valid moves the DQN has made this game
+				if (!c4board.isGameOver())										// if the game is not over...
+				{
+					c4board.addPiece(action, 1);						// ...tell c4board to have player 1 move there
+					theBoard.makeMove(action, SolverState.X);					// ...and tell solverBoard about it
+					if (c4board.isGameOver())									// ...then if the game is over...
+					{
+						r = c4board.getFinalScore() * scaleFactor;				// ...assign final score
+					}
+					if (v)														//  print the resulting board state and adjust column numberings for humans
+					{
+						int columnAdj = action + 1;
+						System.out.println("DQN (X) adds piece to column: " + columnAdj);
+						System.out.println(c4board.toString());
+					}
+				}
 
-                if (c4board.isGameOver() == true)
-                {
-                    r = c4board.getFinalScore() * scaleFactor;
-                }
-            }
+				/*
+					Solver moves next
+				*/
+				if (!c4board.isGameOver())										// if the game is still not over
+				{
+					//  ...get the Solver move, then update the Solver board, then make the move in the c4board
+						SolverGamePlay computerMove = computerPlayer.getNextMove(theBoard);
+						theBoard.makeMove(computerMove.col, SolverState.O);
+						c4board.addPiece(computerMove.col, 2);
 
+					if (v)														// ...print the resulting board state and adjust column numberings for humans
+					{
+						System.out.println("Solver (O) added piece to column: " + (computerMove.col + 1));
+						System.out.println(c4board.toString());
+					}
 
-            if (c4board.isGameOver() == false)
-            {
-                SolverGamePlay computerMove = computerPlayer.getNextMove(theBoard);
-                theBoard.makeMove(computerMove.col, SolverState.O);
-                c4board.addPiece(computerMove.col, 2);
+					if (c4board.isGameOver() == true)							// ...then if the game is over...
+					{
+						r = c4board.getFinalScore() * scaleFactor * -1;			// ...assign final score
+					}
+				}
 
-                if (c4board.isGameOver() == true)
-                {
-                    r = c4board.getFinalScore() * scaleFactor * -1;
-                }
-            }
+				if (c4board.isGameOver())										// if game is over...
+				{
+					if (v)														//  ... print moves
+					{
+						int totalMoves = c4board.getDqnInvalid() + c4board.getDqnValid();
+						System.out.print(this.stepCount + "\ttotal steps\t\t" + totalMoves + "\ttotal moves\t\t\t\t\t\t\t\t\t");
+					}
+					isDone();
+				}
+			}
+			else 																// if move is illegal (column full)...
+			{
+				c4board.setDqnInvalid(c4board.getDqnInvalid()+1);				// ...increment DQN invalid move count
+				r= -500;														// ...set reward to train it not to do that
 
-            else if (c4board.isGameOver() == true)
-            {
-                int totalMoves = c4board.getDqnInvalid()+c4board.getDqnValid();
-                System.out.print(this.stepCount + "\ttotal steps\t\t" + totalMoves + "\ttotal moves\t\t\t\t\t\t\t\t\t");
-                isDone();
-            }
-            //System.out.println(c4board.toString());
-        }
-        else // if move is illegal
-        {
-            //System.out.println("Illegal move! Avail cols: " + Arrays.toString(legalActions) + "\tYou picked col " + action);
-            c4board.setDqnInvalid(c4board.getDqnInvalid()+1);
-            //c4board.setnIllegal(c4board.getnIllegal()+1);
-            r= -1000;
-            //c4board.setGameOver(true);
-        }
+				if (v)															//  ...print illegal attempt
+				{
+					int columnAdj = action + 1;
+					System.out.println("Illegal move! DQN (X) tries to add piece to column " + columnAdj);
+				}
+				c4board.setGameOver(true);										// ...end the game
+			}
 
         actions = new int[c4board.getLegalActions().length];
         actions = c4board.getLegalActions();
-        //System.out.println();
 
         return new StepReply(new GameObservation(c4board), r, c4board.isGameOver(), null);
     } // end of .step()
--- java/connect4ever/hardware_connect.java
+++ java/connect4ever/hardware_connect.java
@@ -1,4 +1,4 @@
-package Legacy;
+package connect4ever;
 
 import com.fazecast.jSerialComm.SerialPort;
 

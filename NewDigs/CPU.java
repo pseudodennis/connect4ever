f2202e10b97f4268ad52ec20e1f590d069d416f3
hjpoe <35583004+hjpoe@users.noreply.github.com>
2018-05-06 14:15:26 -0400

    Javadoc Comments

--- NewDigs/CPU.java
+++ NewDigs/CPU.java
@@ -1,11 +1,21 @@
 import java.util.*;
 
+/**
+ * The CPU class is a computer player that picks random columns to move in.
+ */
+
 public class CPU {
 
 	public CPU() {
 
 
 	}
+	
+	/**
+	 * The move method picks a random and valid column on the board.
+	 * @param playerBoard The current state of the board
+	 * @return a random column
+	 */
 
 	public int move(int[][] playerBoard) {
 
@@ -22,6 +32,6 @@ public class CPU {
 		hardware_connect.sendNum(colPick + 1);
 		System.out.println(colPick);
 		return colPick;
-	}
+	} // end of move
 
-}
+} // end of CPU
